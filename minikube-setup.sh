cb986b359143bc48a71031eb0d503c86b5159c17
Sriram Kajjayam <69500852+Kajjayamsriram@users.noreply.github.com>
2026-06-10 11:35:38 +0530

    Update minikube-setup.sh

--- minikube-setup.sh
+++ minikube-setup.sh
@@ -11,3 +11,8 @@ minikube start --driver=docker --force
  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
  kubectl version --client
+
+##Install kops
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
