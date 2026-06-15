0b24947870c3be9c22d40c708f090bc02c220f22
Sriram Kajjayam <69500852+Kajjayamsriram@users.noreply.github.com>
2026-06-15 11:58:25 +0530

    Update minikube-kops-setup.sh

--- minikube-kops-setup.sh
+++ minikube-kops-setup.sh
@@ -16,3 +16,7 @@ minikube start --driver=docker --force
 curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops
 sudo mv kops /usr/local/bin/kops
+
+##Kubens
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
