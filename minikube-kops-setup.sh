3bbd0a42e416d635b42966608e02b41f5e7f318c
Sriram Kajjayam <69500852+Kajjayamsriram@users.noreply.github.com>
2026-06-23 21:00:46 +0530

    Update with helm install

--- minikube-kops-setup.sh
+++ minikube-kops-setup.sh
@@ -20,3 +20,8 @@ sudo mv kops /usr/local/bin/kops
 ##Kubens
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+#Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4
+chmod 700 get_helm.sh
+./get_helm.sh
