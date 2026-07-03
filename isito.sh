347f84712df6878faf76d6c9360ebb1c31fd160b
Sriram Kajjayam <69500852+Kajjayamsriram@users.noreply.github.com>
2026-07-03 18:30:40 +0530

    Create isito.sh

---
+++ isito.sh
@@ -0,0 +1,25 @@
+#install istio
+curl -L https://istio.io/downloadIstio | sh -
+cd istio-1.30.2
+export PATH=$PWD/bin:$PATH
+
+#Enable default ns for injecting envoy containers
+kubectl label namespace default istio-injection=enabled
+
+#Using demo profile
+istioctl install --set profile=demo -y
+
+#Expose svc's
+kubectl patch svc productpage -p '{"spec": {"type": "LoadBalancer"}}'
+#Note: url:9080/productpage to view page
+
+#Addons
+kubectl apply -f samples/addons
+# Expose Kiali Dashboard
+kubectl patch svc kiali -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
+# Expose Grafana Dashboard
+kubectl patch svc grafana -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
+# Expose Prometheus Metrics
+kubectl patch svc prometheus -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
+# Expose Jaeger Tracing (via "tracing" service)
+kubectl patch svc tracing -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
