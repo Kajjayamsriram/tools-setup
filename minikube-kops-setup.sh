3fe7dadac5032948069c4308c048fd9dea641596
Sriram Kajjayam <69500852+Kajjayamsriram@users.noreply.github.com>
2026-07-03 11:01:00 +0530

    Update minikube-kops-setup.sh

--- minikube-kops-setup.sh
+++ minikube-kops-setup.sh
@@ -6,6 +6,9 @@ sudo rpm -Uvh minikube-latest.x86_64.rpm
 yum install docker -y && systemctl start docker
 minikube start --driver=docker --force
 
+#Network policies to work
+minikube start --cni=cilium --force
+
 ##kubectl install
  curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
  sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
