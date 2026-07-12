d4dd1c6129fccd7b1f183ed51c3245e384a2f1e5
Sriram Kajjayam <69500852+Kajjayamsriram@users.noreply.github.com>
2026-07-12 16:10:23 +0530

    Update compose-install.sh

--- compose-install.sh
+++ compose-install.sh
@@ -1,13 +1,3 @@
-# Download the current stable release of Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-# Apply executable permissions
+sudo curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-# Create a symbolic link
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Install using pip (Python package manager)
-sudo yum install -y python3-pip
-sudo pip3 install docker-compose
-
-#check version
 docker-compose --version
